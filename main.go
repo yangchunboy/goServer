492624cce001aeacd06ca36c479c9f08ff3f1e1e
shen chuck <121477964@qq.com>
2021-05-29 11:26:45 +0800

    将路由都移到了router里面

--- main.go
+++ main.go
@@ -10,10 +10,5 @@ import (
 func main() {
 	ginRouter := gin.Default()
 	router.Router(ginRouter)
-	ginRouter.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 	ginRouter.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
